c8b781032f507c5e70dd89e9a99a8f5af525e083
Muhammad Amir <aamir569696@gmail.com>
2026-08-02 08:44:05 +0500

    "project Update"

--- Frontend/perplexcity-frontend/src/main.jsx
+++ Frontend/perplexcity-frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './App/index.css'
-import App from './App/App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./App/index.css";
+import App from "./App/App.jsx";
+import { store } from "./App/app.store.js";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
-)
+);
