c7c3a2c5cc17c81e37da15ad05408da9585c84d7
Muhammad Amir <aamir569696@gmail.com>
2026-08-08 08:42:24 +0500

    add chat and message models, integrate Mistral AI for title generation and response handling

--- Backend/src/controllers/chat.controller.js
+++ Backend/src/controllers/chat.controller.js
@@ -1,5 +1,46 @@
+import { generateResponse, generateTtile } from "../services/ai.service.js";
+import ChatModel from "../models/chat.model.js";
+import MessageModel from "../models/message.model.js";
 
 export async function sendMessage(req, res) {
-    const {message}=req.body;
-    console.log("Received message:", message);
+  const { message, chat: chatId } = req.body;
+
+  let chat = null;
+  let title = null;
+
+  if (!chatId) {
+    title = await generateTtile(message);
+    console.log("Generated title:", title);
+    chat = await ChatModel.create({
+      user: req.user.id,
+      title: title,
+    });
+  }
+
+  const userMessage = await MessageModel.create({
+    chat: chatId || chat._id,
+    content: message,
+    role: "user",
+  });
+
+  const messages = await MessageModel.find({chat:chatId})
+
+  const result = await generateResponse(messages);
+
+  console.log("Messages in chat:", messages);
+
+  const aiMessage = await MessageModel.create({
+    chat: chatId || chat._id,
+    content: result,
+    role: "assistant",
+  });
+
+  res.json({
+    geminiMessage: result,
+    title,
+    chat,
+    aiMessage,
+  });
+
+ // console.log("Received message:", message);
 }
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,12 +1,39 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { HumanMessage,SystemMessage,AIMessage } from "langchain";
+import { ChatMistralAI } from "@langchain/mistralai";
 
-const model = new ChatGoogleGenerativeAI({
+
+const GeminiModel = new ChatGoogleGenerativeAI({
   model: "gemini-flash-latest",
   apiKey: process.env.GEMINI_API_KEY,
 });
 
-// export async function testGemini(req, res) {
-//   await model.invoke("what is AI explain under 10 words?").then((response) => {
-//     console.log(response.text);
-//   });
-// }
+const MistralModel = new ChatMistralAI({
+  model: "mistral-small-latest",
+  apiKey: process.env.MISTRAL_API_KEY,
+});
+
+export async function generateTtile(message) {
+  const response = await MistralModel.invoke([
+    new SystemMessage(
+      "You are a helpful assistant that generates a title for the given message. The title should be concise, relevant, and capture the essence of the message.",
+    ),
+    new HumanMessage(`
+      generate a title for the following message: "${message}". The title should be concise, relevant, and capture the essence of the message and 2 to 3 word length. Please provide only the title without any additional text or explanation.
+      `),
+  ]);
+
+  return response.text;
+}
+
+export async function generateResponse(messages) {
+  const response = await GeminiModel.invoke(messages.map ((msg)=>{
+    if(msg.role === 'user'){
+      return new HumanMessage(msg.content);
+    }else if(msg.role === 'assistant'){
+      return new AIMessage(msg.content);
+    }
+  })  );
+
+  return response.text;
+}
